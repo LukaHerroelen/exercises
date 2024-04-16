c6c84a5fd960d4071ee23f58d8fd4d7dc5634527
LukaHerroelen <luka.herroelen@student.ucll.be>
2024-04-16 08:50:42 +0200

    oef

--- 01-basic-python/02-conditionals/03-between/student.py
+++ 01-basic-python/02-conditionals/03-between/student.py
@@ -1,1 +1,4 @@
 # Write your code here
+def between(x, lower, upper):
+    return x >= lower and x <= upper
+        
--- 01-basic-python/03-loops/02-is-prime/student.py
+++ 01-basic-python/03-loops/02-is-prime/student.py
@@ -1,1 +1,8 @@
-# Write your code here
+    # Write your code here
+def is_prime(n):
+    i = 2
+    while i < n:
+       if n%i == 0:
+           return False
+       i += 1 
+    return n>1
